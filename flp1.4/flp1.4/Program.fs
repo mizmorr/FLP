5243f8880961277830eb885924224f59b3faf3c3
mizmorr <safonov.ilya22@gmail.com>
2022-03-12 23:19:30 +0300

    1.4/1.5 create

--- flp1.4/flp1.4/Program.fs
+++ flp1.4/flp1.4/Program.fs
@@ -1,4 +1,20 @@
 ﻿
+let sum a b =
+    a+b
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let mult a b =
+    a*b
+
+//let del a func acc =
+//    match a with
+//    |acc when acc<sqrt(a) -> 
+//        match acc with
+//        |acc when 
+
+
+let rec  del a acc =
+    match a with
+    |a when a>0 ->
+        match acc with
+        |acc when a%acc=0-> acc+ del a acc-1
+        |acc->
