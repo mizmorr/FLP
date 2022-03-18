988a8a3c42402f7569d733af33e8e32ad0a077ad
mizmorr <safonov.ilya22@gmail.com>
2022-03-18 22:11:59 +0300

    .

---
+++ lab5/flp1.2/fl1.2/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+let func1 a =
+    match a with
+    |a when a ="Prolog" ->"sneaky"
+    |a when a ="F#"->"You are podliza"
+    |"Python" ->"-rep"
+    |"C" ->"+rep"
+    | _ ->"idk"
+
+let write a =
+    printfn a
+
+let  ob =   
+    
+[EntryPointAttribute]
+
+let main func =
+   
+
+    
+
+     
+
+
+
+   
+0
+        
