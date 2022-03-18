51d36414720dab5d079e6941d9c006156fc78848
mizmorr <safonov.ilya22@gmail.com>
2022-03-18 21:52:44 +0300

    .

--- lab5/flp1.4/flp1.4/Program.fs
+++ lab5/flp1.4/flp1.4/Program.fs
@@ -13,8 +13,31 @@ let mult a b =
 
 
 let rec  del a acc =
-    match a with
-    |a when a>0 ->
-        match acc with
-        |acc when a%acc=0-> acc+ del a acc-1
-        |acc->
+    match acc with
+    |acc when acc>0 -> 
+      match acc with  
+        |acc when a%acc=0 -> acc + del a acc-1
+        |acc -> del a acc-1
+    |acc-> 0
+
+
+//let rec ddel a ac1 =
+//    let d = sqrt(a)
+//    match a with
+//    |a when System.Convert.ToDouble(a)%10=2
+
+let rec ddel a ac =
+    match ac with
+    |ac when ac<sqrt(a) ->
+        match ac with   
+        |ac when System.Convert.ToDouble(a)%ac=0 -> System.Convert.ToDouble(ac) +ddel a ac+1
+
+
+let sda d =
+    del d d-1
+
+[<EntryPointAttribute>]
+
+let main arg = 
+    printf"%d" (sda 6)
+    0
