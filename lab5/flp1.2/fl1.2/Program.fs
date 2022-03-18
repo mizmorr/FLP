d2303397cc4de1ac763721f51cc10450e20e6393
mizmorr <safonov.ilya22@gmail.com>
2022-03-19 01:54:56 +0300

    processing

--- lab5/flp1.2/fl1.2/Program.fs
+++ lab5/flp1.2/fl1.2/Program.fs
@@ -1,28 +1,32 @@
 ﻿open System
-let func1 a =
+let func1  a =
+    printfn "%s"("fav lang?") 
     match a with
-    |a when a ="Prolog" ->"sneaky"
-    |a when a ="F#"->"You are podliza"
-    |"Python" ->"-rep"
-    |"C" ->"+rep"
-    | _ ->"idk"
+    |a when a ="Prolog" -> "sneaky"
+    |a when a ="F#"-> "You are podliza"
+    |"Python" ->   "-rep"
+    |"C" ->  "+rep"
+    | _ -> "idk"
 
-let write a =
-    printfn a
+let write a   =
+    printfn "%A" a 
 
-let  ob =   
+let  ob = Console.ReadLine()
     
-[EntryPointAttribute]
+[<EntryPoint>]
 
-let main func =
-   
-
-    
-
-     
-
-
-
-   
+let main func  =  
+    let fff a =
+        match a with
+        |a when System.String.IsNullOrEmpty(a.ToString())=false -> write(func1 a)
+        |_ -> ( )
+    fff ob
 0
-        
+
+//let main func  =
+//    let func2 a  = 
+//         a+5
+//    func2 5
+
+
+    
