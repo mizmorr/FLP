653cc888b201024f267faa6f633092e97193f002
mizmorr <safonov.ilya22@gmail.com>
2022-03-21 21:37:32 +0300

    upd

--- lab5/flp1.4/flp1.4/Program.fs
+++ lab5/flp1.4/flp1.4/Program.fs
@@ -1,53 +1,21 @@
 ﻿open System
-//let sum a b =
-//    a+b
 
-//let mult a b =
-//    a*b
-
-////let del a func acc =
-////    match a with
-////    |acc when acc<sqrt(a) -> 
-////        match acc with
-////        |acc when 
-
-
-//let rec  del a acc =
-//    match acc with 
-//    |acc when acc<a/2 ->
-//        match acc with
-//        |acc when a%acc=0 -> acc + del a acc+1
-//        |_ -> del a acc+1
-//    |_-> acc
-
-      
-//let func a =
-//    del a 0
+let rec del (func:int->int->int)  a acc =  
+    let z = acc
+    match acc with
+    |acc when acc<a ->
+        match acc with
+        |acc when a%acc=0 ->
+            func acc z
+            del func a (acc+1)
+        |_->del func a (acc+1)
 
 
-//let write d = 
-//    printfn "%i" (func d)
-
-//let main a = 
-//    let z= printf "ad"
-//    z
-
- //let rec ddel a ac1 =
- //    let d = sqrt(a)
- //    match a with
- //    |a when System.Convert.ToDouble(a)%10=2
- 
- //let rec ddel a ac =
- //    match ac with
- //    |ac when ac<sqrt(a) ->
- //        match ac with   
- //        |ac when System.Convert.ToDouble(a)%ac=0 -> System.Convert.ToDouble(ac) +ddel a ac+1
- 
- let write a =
-     printfn "%A" a
-     
-[<EntryPoint>]
- let main af = 
-   printfn "%s" "asd"
-   System.Console.ReadKey()
-   0
+let print a b = 
+    printfn "%i" (a)
+    printfn "%i" (b)
+let s a b = a+b 
+[<EntryPointAttribute>]
+let main arg =
+    printfn "%i " (del s 6 1)
+    0
