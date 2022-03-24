b41f8680f3cc0202a8ff0364c03eb437de9f9349
mizmorr <safonov.ilya22@gmail.com>
2022-03-24 03:00:32 +0300

    final upd

--- lab5/flp1.6/flp1.6/Program.fs
+++ lab5/flp1.6/flp1.6/Program.fs
@@ -1,4 +1,26 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec gcd x y =
+    match y with
+    |y when y=0 -> x 
+    |_-> gcd y (x%y)
+
+let IsCoprIntg x y = gcd x y = 1
+
+let Euler a = 
+    let rec eEuler a b (value:int) =
+        match b with
+        |b when b = 0 -> value 
+        |_->
+            let aValue = 
+                match a with
+                |a when IsCoprIntg a b -> value+1
+                |_-> value
+            let nB = b-1
+            eEuler a nB aValue
+    eEuler a (a-1) 0
+
+[<EntryPoint>]
+let main arg = 
+    printfn"%i" (Euler 4)
+    0
