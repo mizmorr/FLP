9ad007a6ba9161c9cef718b38f9c55e296a89574
mizmorr <safonov.ilya22@gmail.com>
2022-03-31 20:07:52 +0300

    finalupd

---
+++ lab6/flp1.46/flp1.46/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+let mm a b =
+    match a with
+    |a when a>b -> b
+    |_-> a
+let rec readlist n =
+    match n with
+    |n when n=0 -> []
+    |_->
+        let Head = System.Convert.ToInt32(Console.ReadLine())
+        let Tail = readlist (n-1)
+        Head::Tail
+let readData =
+    let n =  System.Convert.ToInt32(Console.ReadLine())
+    readlist n
+let rec listSum list1 list2=
+    match list1 with
+    |[] ->list2
+    |h::t ->listSum t (h::list2)
+
+let relist list =
+    let rec rl list list1 list2=
+        match list with
+        |[] ->listSum list1 list2
+        |h::t ->    
+                if h>=0 then rl t (h::list1) list2
+                else rl t list1 (h::list2)
+    rl list [] []
+
+[<EntryPoint>]
+let main arg = 
+    printfn "%A" (readData)
+    printfn "%A" (relist readData)
+    0
+
