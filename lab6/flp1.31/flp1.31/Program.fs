506a57f01c76bbba07dcb54a8319719f49b3ea99
mizmorr <safonov.ilya22@gmail.com>
2022-03-31 22:54:36 +0300

    final upd

---
+++ lab6/flp1.31/flp1.31/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+let mm a b =
+    match a with
+    |a when a>b -> b
+    |_-> a
+let rec readlist n =
+    match n with
+    |n when n=0 -> []
+    |_->
+        let Head = System.Convert.ToInt32(Console.ReadLine())
+        let Tail = readlist (n-1)
+        Head::Tail
+let readData =
+    let n =  System.Convert.ToInt32(Console.ReadLine())
+    readlist n
+
+let rec check (p:int->bool) list value =
+    match list with
+    |[]->value
+    |h::t->
+        if p h then check p t (value+1)
+        else check p t value
+let IsEven x = x%2=0
+let NumbEven list =
+    check IsEven list 0
+
+[<EntryPoint>]
+let main agr = 
+    printfn "%A" (readData)
+    printfn "%i" (NumbEven readData)
+    0
+    
