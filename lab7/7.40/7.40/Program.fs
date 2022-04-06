11ffbace7673442590817857ca323c916a464565
mizmorr <safonov.ilya22@gmail.com>
2022-04-07 00:15:47 +0300

    40 done

--- lab7/7.40/7.40/Program.fs
+++ lab7/7.40/7.40/Program.fs
@@ -1,3 +1,23 @@
 ﻿open System
 
+let rec rL n = 
+    if n=0 then []
+    else
+    let h = System.Convert.ToInt32(System.Console.ReadLine())
+    let t = rL (n-1)
+    h::t
 
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    rL n
+
+
+let f40 list= List.min (List.filter(fun x->x%2=0) list)
+    
+
+[<EntryPoint>]
+let main arg =
+    let list = readData
+    printfn "%A" list
+    printfn"Min even - %i" (f40 list)
+    0
