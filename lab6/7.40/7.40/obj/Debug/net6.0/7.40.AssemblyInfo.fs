c73ed5324d22afede83054e88885d2fb194551ac
mizmorr <safonov.ilya22@gmail.com>
2022-04-07 00:01:40 +0300

    bad wfixed

--- lab6/7.40/7.40/obj/Debug/net6.0/7.40.AssemblyInfo.fs
+++ lab6/7.40/7.40/obj/Debug/net6.0/7.40.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("7.20")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("7.40")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("7.20")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("7.20")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("7.40")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("7.40")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
