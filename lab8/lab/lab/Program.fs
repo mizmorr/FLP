30df10bba7edc44b21f3cc1f73212d8bd8be0b85
mizmorr <safonov.ilya22@gmail.com>
2022-04-19 23:50:15 +0300

    task8 - done

--- lab8/lab/lab/Program.fs
+++ lab8/lab/lab/Program.fs
@@ -3,13 +3,43 @@ open System.Diagnostics
 open System.Text.RegularExpressions
 
 
-type Passport ( name:string, surname:string, number:int, birthday:DateTime, birthPlace:string, series:int ) =
-    member this.name =name
-    member this.surname =surname 
-    member this.number =number
-    member this.series = series
-    member this.birthday = birthday
-    member this.birthPlace =birthPlace
+type Passport (name:string, surname:string, number:int, birthday:string, birthPlace:string, series:int) =
+    member this.name 
+        with get()=name 
+        and set(testName:string) = 
+                if (Regex.IsMatch(testName, @"[A-Za-z]"))
+                then this.name<-testName
+                else printfn "%s"("bad format")
+    member this.surname 
+                    with get()=surname
+                    and set(testSn:string) =
+                            if (Regex.IsMatch(testSn,@"[A-Za-z]"))
+                            then this.surname<-testSn
+                            else printfn "%s"("bad format")
+    member this.number 
+                   with get()= number
+                   and set(testNumber:int) =
+                           if (Regex.IsMatch(testNumber.ToString(),@"[0-9]{6}"))
+                           then this.number<-testNumber
+                           else printfn "%s"("bad format")
+    member this.series 
+                   with get()=series
+                   and set(testSeries:int) =
+                           if (Regex.IsMatch(testSeries|>string,@"[0-9]{4}"))
+                           then this.series<-testSeries
+                           else printfn "%s"("bad format")
+    member this.birthday 
+                   with get() = birthday
+                   and set(testBday:string) =
+                           if(Regex.IsMatch(testBday,@"[1-30]:[1-12]:[1900-2022]"))
+                           then this.birthday<-testBday
+                           else printfn "%s"("bad format")
+    member this.birthPlace
+                    with get() =birthPlace
+                    and set(testbirthPlace:string) =
+                            if (Regex.IsMatch(testbirthPlace,@"[A-Za-z]+"))
+                            then this.birthPlace<-testbirthPlace
+                            else printfn "%s"("bad format")
     override this.Equals(a) =
         match a with    
         | :? Passport as p -> (p.number)=(this.number)&&(p.series)=(this.series)
