e9dc228c6de1823c56dd19f490a12bd23d86f4a9
mizmorr <safonov.ilya22@gmail.com>
2022-04-27 23:39:35 +0300

    lab6 - 1.12 done

---
+++ lab9/lab6/Libr1/Libr1/Main.fs
@@ -0,0 +1,38 @@
+﻿module Design.Main
+open System
+open Program
+open System.Windows.Forms
+open System.Drawing
+[<EntryPoint; STAThread>]
+let main argv =
+    Application.EnableVisualStyles()
+    Application.SetCompatibleTextRenderingDefault(false)
+    use form = new StartForm()
+    form.Text<-"MmMMassive"
+    let text= new RichTextBox(Font=new Font(familyName="Comic Sans MS",emSize = 12.0f),Text ="\n\tInput massive: \n\t*without last _ ", Width=200,Height=75,ReadOnly=true,Left=250,BackColor=Color.AntiqueWhite)
+    let mass = new TextBox(Width=200,Height=25,Left =250,Top=90, BackColor=Color.AntiqueWhite )
+    let btnsolve=new Button(Font=new Font(familyName="Comic Sans MS",emSize = 14.0f),Text="SoLVE", BackColor=Color.AntiqueWhite, Left=500,Top=200,Height=150,Width=150)
+    let answtext=new RichTextBox(Width=200,Height=25,Left =250,Top=200, BackColor=Color.AntiqueWhite,ReadOnly=true)
+    let answbox = new RichTextBox(Width=200,Height=50,Left =250,Top=125, BackColor=Color.AntiqueWhite,ReadOnly=true, Text="\tComPlEte!!\n(reverse between max min)",Font=new Font(familyName="Comic Sans MS",emSize = 12.0f))
+    let _=btnsolve.Click.Add((fun (e:EventArgs)->
+                    let prepreres=mass.Text.Split(' ')
+                    let preres =List.map System.Int32.Parse (Seq.toList(prepreres))
+                    let res = MakeResList preres
+                    let  ListToString l =
+                        let rec tr s l =
+                            match l with
+                            |[]->s
+                            |h::t ->
+                                tr (s+h.ToString()+" ") t
+                        tr "" l
+                    let ss=ListToString res
+                    answtext.Text<-ss
+                    btnsolve.Dispose()
+                    form.Controls.Add(answbox)
+                    ))
+    form.Controls.Add(text)
+    form.Controls.Add(mass)
+    form.Controls.Add(btnsolve)
+    form.Controls.Add(answtext)
+    Application.Run(form)    
+    0 
---
+++ lab9/lab6/Libr1/Libr1/Program.fs
@@ -0,0 +1,96 @@
+﻿module Program
+open System
+let mm a b =
+    match a with
+    |a when a>b -> b
+    |_-> a
+let rec readlist n =
+    match n with
+    |n when n=0 -> []
+    |_->
+        let Head = System.Convert.ToInt32(Console.ReadLine())
+        let Tail = readlist (n-1)
+        Head::Tail
+let readData =
+    let n =  System.Convert.ToInt32(Console.ReadLine())
+    readlist n
+let rec check (func:int->int->int) list value =
+    match list with
+    |[]->value
+    |h::t->
+        let newvalue = func value h
+        check func t newvalue
+let listMin list = 
+    match list with 
+    |[] -> 0
+    |h::t ->
+        check mm list h
+let max a b =
+    match a with
+    |a when a>b->a
+    |_->b
+
+let ListMax list =
+    match list with
+    |[]->0
+    |h::t ->
+        check max list h
+
+let reverse (list:int list) =
+    let rec r (list:int list) (list1:int list) =
+        match list with
+        |[] ->list1
+        |h::t -> 
+            let newlist1 = h::list1
+            r t newlist1
+    r list []
+
+let centralpiece list =
+    let min=listMin(list)
+    let max=ListMax(list)
+    let rec prl list list1 pointer= 
+        match list with
+        |[]->list1
+        |h::t ->
+           match pointer with
+            |0 -> 
+                    if h<>min then prl t list1 0
+                    else prl t list1 1
+           
+            |1 -> if h<>max then prl t (h::list1) 1
+                    else prl t list1 2
+            |2-> prl t list1 pointer
+    prl list [] 0
+let firstpiece list =
+    let min=listMin(list)
+    let rec fp list list1 pointer =
+        match list with
+        |[]->list1
+        |h::t->
+            match pointer with
+            |0 -> if h<>min then fp t (h::list1) pointer
+                     else fp t (h::list1) 1
+            |1-> fp t list1 pointer
+    fp list [] 0
+
+let thirdpiece list =
+    let max=ListMax(list)
+    let rec tp list list1 pointer =
+        match list with
+        |[]->list1
+        |h::t ->
+                match pointer with 
+                |0 -> if h<>max then tp t list1 pointer
+                         else tp t (h::list1) 1
+                |1->tp t (h::list1) pointer
+    tp list [] 0
+     
+let rec pluslist list list2 = 
+    match list with
+    |[] ->list2
+    |h::t ->pluslist t (h::list2)
+let MakeResList list=
+    let list1=firstpiece(list)
+    let list2=centralpiece(list)
+    let list3=reverse(thirdpiece(list))
+    pluslist (pluslist list2 list1) list3
