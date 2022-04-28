f58a6255de514fb2b6abd6bf21343391f1b93c96
mizmorr <safonov.ilya22@gmail.com>
2022-04-28 23:58:09 +0300

    part2 - done

--- lab8/lab4/lab4/Program.fs
+++ lab8/lab4/lab4/Program.fs
@@ -20,16 +20,41 @@ type Rectangle(lenght:int,width:int) =
     override this.ToString()="Rectangle:\n"+"Lenght: "+lenght.ToString()+"\nWidth:"+width.ToString()+"\nArea: "+(width*lenght).ToString()
     interface  IPrint with
         override this.Print(): unit=Console.WriteLine(this.ToString())
-type Round(radius:int) =
+type Circle(radius:int) =
     member this.radius 
         with get()=radius
         and set(r:int)=this.radius<-r
-    override this.ToString()="Round:\n"+"Radius: "+radius.ToString()+"\nArea: "+(2*int(Math.PI)*radius).ToString()
+    override this.ToString()="Round:\n"+"Radius: "+radius.ToString()+"\nArea: "+(int(sqrt(Math.PI))*radius).ToString()
     interface  IPrint with
         override this.Print(): unit=Console.WriteLine(this.ToString())
 
     
+//p2
 
+type GeometryFigure = 
+    |Square of int
+    |Rectangle of int*int
+    |Circle of int
+
+let chooser (leng:int) (width:int) (num:int) = 
+    match num  with
+    |1->Square leng
+    |2->Rectangle(leng,width)
+    |3->Circle leng
+
+let Area l w num =
+    let figure = chooser l w num
+    let area =
+        match figure with
+        |Square(a) -> a*a
+        |Rectangle(w,l) ->w*l
+        |Circle(r) -> int(sqrt(Math.PI))*r
+    area
+    
+[<EntryPoint>]
+let main arg = 
+    printf "%i" (Area 2 2 1)
+    0
 
 
     
