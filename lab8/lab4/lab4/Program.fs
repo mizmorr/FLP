a7d0947df7545cf5156374f8a11914b570366035
mizmorr <safonov.ilya22@gmail.com>
2022-04-28 23:41:51 +0300

    part1-done

---
+++ lab8/lab4/lab4/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+
+type IPrint = interface
+    abstract member Print: unit->unit
+    end
+type Square(lenght:int)=
+    member this.lenght 
+        with get()=lenght
+        and set(leng:int)=this.lenght<-leng
+    override this.ToString()="Square:\n"+"Lenght: "+lenght.ToString()+"\nArea: "+(lenght*lenght).ToString()
+    interface IPrint with
+        override this.Print(): unit=Console.WriteLine(this.ToString())
+type Rectangle(lenght:int,width:int) =
+    member this.lenght 
+        with get()=lenght
+        and set(leng:int)=this.lenght<-leng
+    member this.width 
+        with get()=width
+        and set(wt:int)=this.width<-wt
+    override this.ToString()="Rectangle:\n"+"Lenght: "+lenght.ToString()+"\nWidth:"+width.ToString()+"\nArea: "+(width*lenght).ToString()
+    interface  IPrint with
+        override this.Print(): unit=Console.WriteLine(this.ToString())
+type Round(radius:int) =
+    member this.radius 
+        with get()=radius
+        and set(r:int)=this.radius<-r
+    override this.ToString()="Round:\n"+"Radius: "+radius.ToString()+"\nArea: "+(2*int(Math.PI)*radius).ToString()
+    interface  IPrint with
+        override this.Print(): unit=Console.WriteLine(this.ToString())
+
+    
+
+
+
+    
